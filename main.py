bb2177f0e6f65dd4ceb304630177a2eab19c265d
freeebatch <koushalanurag0@gmail.com>
2026-01-10 13:44:34 +0530

    Change destination chat ID in main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ async def start_batch(c: Client, m: Message):
 
     total_count = 1000
     batch_size = 20
-    dest_chat = "-1003110533278"
+    dest_chat = "-1003650632126"
     sent_success = 0
 
     progress_msg = await m.reply_text("Starting batch… 🐥", quote=True)
